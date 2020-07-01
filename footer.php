01762178532d64f97c80990c67a4d720b85b5a6e
David <dpeters911@hotmail.com>
2020-06-30 19:48:59 -0700

    Updated footer, screenshot

--- footer.php
+++ footer.php
@@ -11,16 +11,16 @@
 	</div>
 
 	<footer id="colophon">
-		<a href="<?php echo esc_url( __( 'https://wordpress.org', 'susty' ) ); ?>">
+		<a href="<?php echo esc_url( __( 'https://wordpress.org', 'agitpress' ) ); ?>">
 			<?php
 			/* translators: %s: CMS name, i.e. WordPress. */
-			printf( esc_html__( 'Proudly powered by %s', 'susty' ), 'WordPress' );
+			printf( esc_html__( 'Proudly powered by %s', 'agitpress' ), 'WordPress' );
 			?>
 		</a>
 		<span> | </span>
 			<?php
 			/* translators: 1: Theme name, 2: Theme author. */
-			printf( esc_html__( 'Theme: %1$s by %2$s.', 'susty' ), '<a href="https://github.com/jacklenox/susty">Susty</a>', '<a href="https://blog.jacklenox.com">Jack&nbsp;Lenox</a>' );
+			printf( esc_html__( 'Theme: Agitpress, a fork of SustyWP' ) );
 			?>
 	</footer>
 </div>
